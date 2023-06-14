7ac5f4c829a5b3bad156aaa67715116aef429dcc
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2023-06-14 23:50:05 +0900

    Re-데이터 파싱

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -13,11 +13,11 @@ class ViewController: UIViewController, UIScrollViewDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loadMovieFromCSV()
-        self.addContentScrollView()
-        self.setPageControl()
-        pageControl.pageIndicatorTintColor = UIColor.gray
-        pageControl.currentPageIndicatorTintColor = UIColor.black
-        setupPopUpButton()
+        //self.addContentScrollView()
+        //self.setPageControl()
+        //pageControl.pageIndicatorTintColor = UIColor.gray
+        //pageControl.currentPageIndicatorTintColor = UIColor.black
+        //setupPopUpButton()
         
         scrollView.delegate = self
     }
@@ -52,7 +52,8 @@ class ViewController: UIViewController, UIScrollViewDelegate{
         do {
             let data = try Data(contentsOf: url)
             let dataEncoded = String(data: data, encoding: .utf8)
-            if let dataArr = dataEncoded?.components(separatedBy: "\n").map({$0.components(separatedBy: "\"[")}) {
+            
+            if let dataArr = dataEncoded?.components(separatedBy: "\n").map({$0.components(separatedBy: "##,")}) {
                 for item in dataArr {
                     movieList.append(item)
                 }
@@ -62,48 +63,18 @@ class ViewController: UIViewController, UIScrollViewDelegate{
         }
         
         for i in 0..<movieList.count - 1 {
-            for j in 0..<10 {
-                if(j == 1 || j==5 || j==7 || j==9) {
+            for j in 0..<12 {
+                if(j == 3 || j == 9 || j == 11) {
                     let data = String(movieList[i][j])
-                    let dataArr = data.components(separatedBy: "]\"")
-                    movieList[i].remove(at: j)
-                    for item in dataArr.reversed() {
-                        movieList[i].insert(item, at: j)
-                    }
-                }
-                if(j == 2) {
-                    let data = String(movieList[i][j])
-                    let dataArr = data.components(separatedBy: "\"")
+                    let dataArr = data.components(separatedBy: "##\",")
                     movieList[i].remove(at: j)
                     for item in dataArr.reversed() {
                         movieList[i].insert(item, at: j)
                     }
                 }
             }
-            
-            for j in 0..<23 {
-                if(j == 0 || j == 4 || j == 10 || j == 16 || j == 22) {
-                    let data = String(movieList[i][j])
-                    let dataArr = data.components(separatedBy: ",")
-                    movieList[i].remove(at: j)
-                    for item in dataArr.reversed() {
-                        movieList[i].insert(item, at: j)
-                    }
-                }
-            }
-            
-            movieList[i].remove(at: 2)
-            movieList[i].remove(at: 3)
-            movieList[i].remove(at: 6)
-            movieList[i].remove(at: 7)
-            movieList[i].remove(at: 8)
-            movieList[i].remove(at: 9)
-            movieList[i].remove(at: 10)
-            movieList[i].remove(at: 13)
-            movieList[i].remove(at: 14)
         }
-        movieList.remove(at: 100)
-        
+        movieList.remove(at: 101)
         movieListInitSet()
     }
     
@@ -120,7 +91,7 @@ class ViewController: UIViewController, UIScrollViewDelegate{
     }
     
     private func loadMovieFromCSV() {
-        let path = Bundle.main.path(forResource: "movies_metadata2", ofType: "csv")!
+        let path = Bundle.main.path(forResource: "movies_metadata3", ofType: "csv")!
         parseCSVAt(url: URL(fileURLWithPath: path))
         //self.tableView.reloadData()
     }
