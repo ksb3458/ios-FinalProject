e641328565500a56f4946cfbf99b4fc967831c57
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2023-06-17 01:29:10 +0900

    Revert "Revert "탭바 (임시)""
    This reverts commit d66c8b40adf88f274d1a552c3c190fcbb0a57d97.

---
+++ FinalProject/TabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  FinalProject
+//
+//  Created by 컴퓨터공학부 on 2023/06/17.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
